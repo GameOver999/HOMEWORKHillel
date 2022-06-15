8dc105ddd5c78e814ce7b8328a1c6172b00dad0b
Rustam <rustamsharay@gmail.com>
2022-06-15 23:09:35 +0300

    -dz dog lesson 4

---
+++ src/main/java/Dog.java
@@ -0,0 +1,57 @@
+public class Dog {
+
+    private String name;
+    private int age;
+    private int height;
+    private String color;
+
+
+    public Dog() {
+        System.out.println("Creating object of the class Dog");
+    }
+
+    public Dog(String name, int age, int height, String color) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.color = color;
+
+    }
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.height = 40;
+        this.color = "Black";
+
+
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void print (){
+        System.out.println("Name "+this.name);
+        System.out.println("Age "+this.age);
+        System.out.println("HEIGHT "+this.height);
+        System.out.println("Color "+this.color);
+    }
+
+
+}
---
+++ src/main/java/HomeWork4DOG.java
@@ -0,0 +1,31 @@
+public class HomeWork4DOG {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Eskobar", 1, 50,"White");
+        Dog dog1 = new Dog("Mosik", 2);
+        Dog dog2 = new Dog();
+        System.out.println("\n");
+
+
+        dog1.print();
+
+
+
+
+
+
+
+
+        //System.out.println(dog1.getColor());
+
+       // System.out.println(dog.getName());
+
+        //dog.setName("Mosik");
+        //System.out.println(dog.getName());
+
+
+
+
+
+
+    }
+}
